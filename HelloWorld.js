1a8dddc35bd284b48a836277a60d5caf5a54c8c9
Akshay <akshaypmishra02@gmail.com>
2020-12-21 00:24:55 +0530

    new with three different types of boxes in js

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,1 +1,13 @@
 document.write("hi we are learning js");
+var x=true;
+var y=false;
+result=x&&y;
+document.write(!result);
+function alertbox()
+    {alert("Hello World!");}
+function promptbox()
+    {prompt("Trying my first prompt-box, try typing someting","anything");}
+
+myFunction(){
+    confirm("Do you want to continue?");
+}
