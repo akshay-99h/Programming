3b7ed6bbaaba0e4bd5605a5c89cb7c2cc66b2ec5
Akshay <akshaypmishra02@gmail.com>
2020-12-24 22:00:48 +0530

    24/12/20

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,12 +1,15 @@
 document.write("hi we are learning js");
 var x=true;
 var y=false;
+if (x>y) {
+    document.write("hello");
+}
 result=x&&y;
 document.write(!result);
 function alertbox()
     {alert("Hello World!");}
 function promptbox()
-    {prompt("Trying my first prompt-box, try typing someting","anything");}
+    {document.write(prompt("Trying my first prompt-box, try typing someting","anything"));}
 
 function myFunction(){
     confirm("Do you want to continue?");
