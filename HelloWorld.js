cf3792e13887884080725e58617a66ba34755853
Akshay <akshaypmishra02@gmail.com>
2020-12-27 00:56:41 +0530

    also added the css files

--- HelloWorld.js
+++ HelloWorld.js
@@ -1,16 +1,20 @@
-document.write("hi we are learning js");
-var x=true;
-var y=false;
-if (x>y) {
-    document.write("hello");
+document.getElementById("hi we are learning js");
+var x = true;
+var y = false;
+if (x > y) {
+  document.getElementById("hello");
 }
-result=x&&y;
-document.write(!result);
-function alertbox()
-    {alert("Hello World!");}
-function promptbox()
-    {document.write(prompt("Trying my first prompt-box, try typing someting","anything"));}
+result = x && y;
+document.getElementById(!result);
+function alertbox() {
+  alert("Hello World!");
+}
+function promptbox() {
+  document.getElementById(
+    prompt("Trying my first prompt-box, try typing someting", "anything")
+  );
+}
 
-function myFunction(){
-    confirm("Do you want to continue?");
+function myFunction() {
+  confirm("Do you want to continue?");
 }
