aa5e255a8cefc75b81f887f98f6eb0bffc8cb279
Akshay <akshaypmishra02@gmail.com>
2020-12-27 18:54:58 +0530

    the one with the maps

--- index.js
+++ index.js
@@ -19,3 +19,13 @@ function promptbox() {
 function myFunction() {
   confirm("Do you want to continue?");
 }
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: {
+      lat: -34.397,
+      lng: 150.644,
+    },
+    zoom: 8,
+  });
+}
