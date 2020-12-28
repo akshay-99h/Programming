39eb7396b4502575a0cfe7e5aa19f529573f62bb
Akshay <akshaypmishra02@gmail.com>
2020-12-28 15:28:14 +0530

    folder management

--- C++ files/5th_Akshay.cpp
+++
@@ -1,8 +0,0 @@
-#include <c++/4.2.1/backward/iostream.h>
-#include <c++/4.2.1/backward/iomanip.h>
-
-int main()  {
-    cout << setw(5) << (double)39.97123456  << endl;
-
-
-}
--- C++ files/Using enum().cpp
+++
@@ -1,7 +0,0 @@
-#include <c++/4.2.1/backward/iostream.h>
-
-    int main(int argc, char const *argv[]) {
-        char a;
-        enum a {north, south, east, west };
-        cout << a.north;
-    }
