4dac7594d09f92db53d33532414ace7a3a60253e
Akshay <akshaypmishra02@gmail.com>
2021-04-29 13:48:19 +0530

    changes I am not sure I made

--- c/Hello_World.c
+++ c/Hello_World.c
@@ -6,5 +6,6 @@
 int main()
 {
     printf("Hello World!");
+    getchar();
     return 0;
 }
---
+++ c/Program-1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    int a=99;
+    printf("%d",a);
+    getchar();
+    return 0;
+}
