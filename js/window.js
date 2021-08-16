ac454b8966ba30b0110b6a13be3bcd07eac2347a
akshay-99h <akshaypmishra02@gmail.com>
2021-08-16 15:06:04 +0530

    learning DOM-initial commit

---
+++ js/window.js
@@ -0,0 +1,17 @@
+console.log("hello");
+console.log(window);
+
+// using document
+
+a = document.links;
+
+b = "google";
+
+a.forEach(function (element)
+{
+  if (string(element).includes(b))
+  {
+    console.log(elemennt);
+  }
+}
+);
